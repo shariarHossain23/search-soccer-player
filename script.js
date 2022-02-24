4853daf881227b0408fd6f9e34e5322d7c70f43f
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-24 12:49:56 +0600

    details

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const playerData = async () =>{
         const res = await fetch(url);
         const data = await res.json();
         showDisplay(data.player)
+        // console.log(data.player[0])
     } catch (error) {
         console.log(error)
     }
@@ -16,7 +17,9 @@ const playerData = async () =>{
 
 const showDisplay =  players =>{
     const display = document.getElementById("display");
+    // clear display
     display.textContent = "";
+    document.getElementById("display-details").textContent = "";
     if(players === undefined && players === null){
         console.log("hi")
     }
@@ -31,9 +34,39 @@ const showDisplay =  players =>{
             <p>position: ${player.strPosition}</p>
             <p>Club: ${player.strTeam}</p>
             <p>Shirt: ${player.strNumber}</p>
+            <button onclick="playerDetails(${player.idPlayer})">details</button>
             `
             display.appendChild(playerDiv)
-            console.log(player)
         });
     }
+}
+ const playerDetails = async details => {
+    try {
+        const url =`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${details}`;
+        const res = await fetch(url);
+        const data = await res.json()
+        playerDetailsDisplay (data.players[0])
+    } catch (error) {
+        console.log(error)
+    }
+ }
+
+
+const playerDetailsDisplay = details =>{
+    const displayDetails = document.getElementById("display-details");
+    // clear display = 
+    displayDetails.textContent = '';
+    document.getElementById("display").textContent = "";
+
+    const detailsDiv = document.createElement("div")
+    detailsDiv.innerHTML = `
+    <img width="300px" src="${details.strThumb}" alt="img not found">
+    <p>Name: ${details.strPlayer}</p>
+    <p>Nationality: ${details.strNationality}</p>
+    <p>sports: ${details.strSport}</p>
+    <p>position: ${details.strPosition}</p>
+    <p>Club: ${details.strTeam}</p>
+    <p>Shirt: ${details.strNumber}</p>
+    `
+    displayDetails.appendChild(detailsDiv)
 }
